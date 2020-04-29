d1c009e3b95fbaa350d244566d755bddb9ec4dda
Martin Zimmer <mz@aeschma.de>
2020-04-29 09:44:04 +0200

    typo in init script

--- bin/dotfiles-init.sh
+++ bin/dotfiles-init.sh
@@ -5,7 +5,7 @@
 
 
 git init --bare $HOME/.git-dotfiles
-alias dotfiles='/usr/bin/git --git-dir=$HOME/.git-dotfiles/ --work-tree=$HOME'
+alias dotfiles='/usr/bin/git --git-dir=$HOME/.git-dotfiles --work-tree=$HOME'
 dotfiles config --local status.showUntrackedFiles no
 dotfiles remote add origin git@git.aeschma.de:mz/dotfiles.git
 echo "alias dotfiles='/usr/bin/git --git-dir=$HOME/.git-dotfiles/ --work-tree=$HOME'" >> $HOME/.zshrc
